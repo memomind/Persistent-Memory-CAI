ef2d08228be3cbdd2b80a76f787fd99e747ea805
Tao Xiang <xiang.tao@outlook.de>
2023-04-07 18:23:37 +0200

    1

--- DM/kg_io.py
+++ DM/kg_io.py
@@ -6,7 +6,7 @@ import toml
 with open('.secrets.toml', 'r') as f:
     secrets = toml.load(f)
 
-graph = Graph("bolt://localhost:7687", auth=(secrets['kg_auth']['username'], secrets['kg_auth']['password']))
+graph = Graph(secrets['kg_auth']['url'], auth=(secrets['kg_auth']['username'], secrets['kg_auth']['password']))
 
 def kg_query(cypher:str) -> List[Dict]:
     return graph.run(cypher).data()
